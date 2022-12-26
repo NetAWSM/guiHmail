088fa73262e2c437bcce293cf0e8c0d07ed6b586
NetAWSM <netroad2ge@mail.ru>
2022-12-26 22:56:41 +0300

    3

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import tkinter as tk
 import psycopg2
 from config import host, user, password, db_name
-import time
 
 
 
 #Коннект к базе
-connection = psycopg2.connect(
-host=host,
-user=user,
-password=password,
-database=db_name
-)
-connection.autocommit = True
+
 
 def hmailServ():
     mail = name.get()
     try:
-        if(mail == "exit"):
-            return
-        elif(len(mail) == 0):
-            return
+        connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name
+        )
+        connection.autocommit = True
+        if(len(mail) == 0):
+            print("Пусто")
         cursor = connection.cursor()
         postgres_insert_query = """ INSERT INTO hm_rule_criterias (criteriaruleid, criteriausepredefined, criteriapredefinedfield, criteriaheadername, criteriamatchtype, criteriamatchvalue) VALUES (%s,%s,%s,%s,%s,%s)"""
         record_to_insert = (1, 1, 2, ' ', 1, mail)
