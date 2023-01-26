433da95d4a44624c153d1f2489dbf5df566d11d6
Net <91050834+NetAWSM@users.noreply.github.com>
2023-01-26 12:38:16 +0300

    update domain

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from configFcaud import hostF, userF, passwordF, db_nameF
 import psycopg2
 
 
-def hmailServ():
+def mailBox():
     #Коннект к базе fnkaud
     connectionFnkaud = psycopg2.connect(
     host=host,
@@ -22,36 +22,82 @@ def hmailServ():
     database=db_nameF
     )
     connectionFcaud.autocommit = True
-    mail = name.get()
+    mail = mail_box.get()
     try:
-        # Функция для fnkaud
         if(len(mail) == 0):
             return
         cursorFnkaud = connectionFnkaud.cursor()
         cursorFcaud = connectionFcaud.cursor()
         postgres_insert_query = """ INSERT INTO hm_rule_criterias (criteriaruleid, criteriausepredefined, criteriapredefinedfield, criteriaheadername, criteriamatchtype, criteriamatchvalue) VALUES (%s,%s,%s,%s,%s,%s)"""
-        record_to_insert = (4, 1, 2, ' ', 2, mail)
-        cursorFnkaud.execute(postgres_insert_query, record_to_insert)
-        cursorFcaud.execute(postgres_insert_query, record_to_insert)
-        print ("Ящик " + mail + " заблокирован на отправку.")
+        record_to_insert_FNKAUD = (1, 1, 2, ' ', 2, mail)
+        record_to_insert_FCAUD = (4, 1, 2, ' ', 2, mail)
+        cursorFnkaud.execute(postgres_insert_query, record_to_insert_FNKAUD)
+        cursorFcaud.execute(postgres_insert_query, record_to_insert_FCAUD)
+        # print ("Ящик " + mail + " заблокирован на отправку.")
+        inf = tk.Label(win, text = "Ящик " + mail + " заблокирован на отправку.")
+        inf.grid(row=6, column=1)
     except Exception as ex:
         print("[INFO] Error while working with PostgreSQL", -ex)
     connectionFnkaud.close()
     connectionFcaud.close()      
+
+def Domain():
+    #Коннект к базе fnkaud
+    connectionFnkaud = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+    )
+    connectionFnkaud.autocommit = True
+
+    #Коннект к базе fcaud
+    connectionFcaud = psycopg2.connect(
+    host=hostF,
+    user=userF,
+    password=passwordF,
+    database=db_nameF
+    )
+    connectionFcaud.autocommit = True
+    mail = domain.get()
+    try:
+        if(len(mail) == 0):
+            return
+        cursorFnkaud = connectionFnkaud.cursor()
+        cursorFcaud = connectionFcaud.cursor()
+        postgres_insert_query = """ INSERT INTO hm_rule_criterias (criteriaruleid, criteriausepredefined, criteriapredefinedfield, criteriaheadername, criteriamatchtype, criteriamatchvalue) VALUES (%s,%s,%s,%s,%s,%s)"""
+        record_to_insert_FNKAUD = (1, 1, 2, ' ', 1, mail)
+        record_to_insert_FCAUD = (4, 1, 2, ' ', 1, mail)
+        cursorFnkaud.execute(postgres_insert_query, record_to_insert_FNKAUD)
+        cursorFcaud.execute(postgres_insert_query, record_to_insert_FCAUD)
+        # print ("Ящик " + mail + " заблокирован на отправку.")
+        inf = tk.Label(win, text = "Домен " + mail + " заблокирован на отправку.")
+        inf.grid(row=6, column=3)
+    except Exception as ex:
+        print("[INFO] Error while working with PostgreSQL", -ex)
+    connectionFnkaud.close()
+    connectionFcaud.close()
 
 
 win = tk.Tk()
 win.geometry("600x200")
 win.title('hmail')
 
-name = tk.Entry(win)
-name.grid(row=1, column=3)
+mail_box = tk.Entry(win)
+mail_box.grid(row=1, column=1)
 
-btn1 = tk.Button(win, text="Отправить", command=hmailServ)
-btn1.grid(row=2, column=3)
+btn1 = tk.Button(win, text="Заблокировать почтовый ящик", command=mailBox)
+btn1.grid(row=2, column=1)
 
-win.grid_columnconfigure(0, minsize=100)
-win.grid_columnconfigure(1, minsize=100)
+domain = tk.Entry(win)
+domain.grid(row = 1, column = 2)
+
+btn2 = tk.Button(win, text="Заблокировать домен", command=Domain)
+btn2.grid(row=2, column=2)
+
+
+# win.grid_columnconfigure(0, minsize=100)
+win.grid_columnconfigure(1, minsize=300)
 
 
 if __name__ == "__main__":
